3d0352f2a1f9dbd0157ef841175d49586d1320c7
AnmolMehra435 <gtrdemon66@gmail.com>
2026-05-22 19:13:54 +0530

    removed cors

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import cors from 'cors'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import analyzeRouter from './routes/analyze.js'
@@ -23,15 +22,6 @@ dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5000
-
-
-app.use(
-  cors({
-    origin: 'http://localhost:5173',
-
-    credentials: true,
-  })
-)
 
 app.use(cookieParser())
 
