79c8cd03e686424329f33c818b3d41931bc9c5d7
AnmolMehra435 <gtrdemon66@gmail.com>
2026-05-22 19:29:51 +0530

    udpated environment variables

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -15,7 +15,7 @@ passport.use(
         process.env.GITHUB_CLIENT_SECRET,
 
       callbackURL:
-        'http://localhost:5000/api/auth/github/callback',
+        'http://13.216.72.49/api/auth/github/callback',
     },
 
     async (
